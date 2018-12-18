93b319a39ea688b7fd9272891bdb7fa0ecd18ef1
오일석 <onestone@live.co.kr>
2018-12-19 06:13:25 +0900

    new index

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,58 +1,336 @@
-'use strict';
+var Protocol = require('azure-iot-device-mqtt').Mqtt;
+var Client = require('azure-iot-device').Client;
+var ConnectionString = require('azure-iot-device').ConnectionString;
+var Message = require('azure-iot-device').Message;
+var async = require('async');
+var BME280 = require('bme280-sensor');
 
-var config = require('./config.json');
-var MessageProcessor = require('./messageProcessor.js');
+var connectionString = 'HostName=s1iotremotemonitoringc9f9c.azure-devices.net;DeviceId=firealam-02.Real;SharedAccessKey=bxCcsNGFZeL86S4CGi87mmf/meBwND0365G1w4cRiSs=';
 
-var clientFromConnectionString = require('azure-iot-device-mqtt').clientFromConnectionString;
+var temperature = 25;
+var temperatureUnit = 'C';
+var humidity = 50;
+var humidityUnit = '%';
 
-var connectionString = config.deviceConnectionString; 
-var client = clientFromConnectionString(connectionString); 
+var temperatureSchema = 'firealarm-temperature;v1';
+var humiditySchema = 'firealarm-humidity;v1';
+var emergencyButtonSchema = 'firealarm-emergencybutton;v1'
+var interval = "00:00:05";
+var buttonInterval = "00:00:01";
+var deviceType = "FireAlarm";
+var deviceFirmware = "1.0.0";
+var deviceFirmwareUpdateStatus = "";
+var deviceLocation = "서울 종로구 종로1길 50 KTT A동 12층";
+var deviceLatitude = 37.5746492;
+var deviceLongitude = 126.9783334;
+var deviceOnline = true;
 
-// Message 
-function handleDisigredSettings(twin) { 
-    twin.on('properties.desired', function (desiredChange) { 
-        // for (let setting in desiredChange) { 
-        //     if (settings[setting]) { 
-        //         console.log('Received setting: ${setting}: ${desiredChange[setting].value}');
-        //         settings[setting](desiredChange[setting].value, (newValue, status, message) => {
-        //             var patch = {
-        //                 [setting]: { 
-        //                     value: newValue,
-        //                     status: status, 
-        //                     desiredVersion: desiredChange.$version, 
-        //                     message: message
-        //                 }
-        //             }
-        //             twin.properties.reported.update(patch, (err) => console.log(`Sent setting update for ${setting}; ` +
-        //             (err ? `error: ${err.toString()}` : `status: success`)));
-        //         });
-        //     }
-        // }
+var alertLEDOn = false;
+var emergencyButtonOn = 0;
+
+// 센서 초기화
+const sensorOptions = {
+    i2cBusNo: 1, // defaults to 1
+    i2cAddress: BME280.BME280_DEFAULT_I2C_ADDRESS() // defaults to 0x77
+};
+const bme280 = new BME280(sensorOptions);
+
+bme280.init()
+    .then(() => {
+        console.log('BME280 initialization succeeded');
+    })
+    .catch((err) => console.error(`BME280 initialization failed: ${err} `));
+
+
+var reportedProperties = {
+    "Protocol": "MQTT",
+    "SupportedMethods": "Reboot,FirmwareUpdate,AlertLEDOn",
+    "Telemetry": {
+        "TemperatureSchema": {
+            "Interval": interval,
+            "MessageTemplate": "{\"temperature\":${temperature},\"temperature_unit\":\"${temperature_unit}\"}",
+            "MessageSchema": {
+                "Name": temperatureSchema,
+                "Format": "JSON",
+                "Fields": {
+                    "temperature": "Double",
+                    "temperature_unit": "Text"
+                }
+            }
+        },
+        "HumiditySchema": {
+            "Interval": interval,
+            "MessageTemplate": "{\"humidity\":${humidity},\"humidity_unit\":\"${humidity_unit}\"}",
+            "MessageSchema": {
+                "Name": humiditySchema,
+                "Format": "JSON",
+                "Fields": {
+                    "humidity": "Double",
+                    "humidity_unit": "Text"
+                }
+            }
+        },
+        "EmergencySchema": {
+            "Interval": buttonInterval,
+            "MessageTemplate": "{\"emergencyButtonOn\":${emergencyButtonOn}}",
+            "MessageSchema": {
+                "Name": emergencyButtonSchema,
+                "Format": "JSON",
+                "Fields": {
+                    "emergencyButtonOn": "Integer"
+                }
+            }
+        },
+    },
+    "Type": deviceType,
+    "Firmware": deviceFirmware,
+    "FirmwareUpdateStatus": deviceFirmwareUpdateStatus,
+    "Location": deviceLocation,
+    "Latitude": deviceLatitude,
+    "Longitude": deviceLongitude,
+    "Online": deviceOnline,
+    "AlertLED": alertLEDOn
+}
+
+function printErrorFor(op) {
+    return function printError(err) {
+        if (err) console.log(op + ' error: ' + err.toString());
+    };
+}
+
+function generateRandomIncrement() {
+    return ((Math.random() * 2) - 1);
+}
+
+function onDirectMethod(request, response) {
+    // Implement logic asynchronously here.
+    console.log('--------Simulated ' + request.methodName);
+
+    // Complete the response
+    response.send(200, request.methodName + ' was called on the device', function (err) {
+        if (err) console.error('Error sending method response :\n' + err.toString());
+        else console.log('200 Response to method \'' + request.methodName + '\' sent successfully.');
     });
 }
 
-var connectCallback = (err) => { 
-    if (err) { 
-        console.log('Device could not connect to Azure IoT Central: ${err.toString()');
-    } else { 
-        console.log('Device successfully connected to Azure IoT Central'); 
-        // 주기적으로 Telemetry 보냄. 
-        setInterval(() => {MessageProcessor.sendSensorData(client);}, config.interval); 
-        // TODO: 보내기 실패시 큐에 넣어서 보관하다가 연결되면 보냄. 
+function onAlertLEDOn(request, response) { 
+    console.log('---------------------  LED ON');
 
-        // Twin 설정 
-        client.getTwin((err, twin) => { 
-            if (err) { 
-                console.log('Error getting device twin: ${err.toString()}');
-            } else { 
-                // 연결되면 한번 디바이스 정보를 보낸다. 
-                MessageProcessor.sendDeviceProperties(twin); 
-                // IoT Central에서 보낸 변경된 설정 값 (fanSpeed, setTemperature)에 대한 처리 핸들러 
-                handleDisigredSettings(twin);
+    // TODO: 실제로 LED 를 제어 함. 
+    alertLEDOn = true;
+
+    var patch = {
+        AlertLED: alertLEDOn
+    };
+    // Twin 업데이트
+    client.getTwin(function (err, twin) {
+        if (!err) {
+            twin.properties.reported.update(patch, function (err) {
+                if (err) console.log("------------reported update error")
+            });
+        } else {
+            if (err) console.log("--------------get twin error")
+        }
+    });
+
+    response.send(200, "Alert LED: " + (alertLEDOn ? "On" : "Off"), function (err) { 
+        if (err) console.error('Error sending method response :\n' + err.toString());
+        else console.log('200 Response to method \'' + request.methodName + '\' sent successfully.');
+    });
+}
+
+function onFirmwareUpdate(request, response) {
+    // Get the requested firmware version from the JSON request body
+    var firmwareVersion = request.payload.Firmware;
+    var firmwareUri = request.payload.FirmwareUri;
+
+    // Ensure we got a firmware values
+    if (!firmwareVersion || !firmwareUri) {
+        response.send(400, 'Missing firmware value', function (err) {
+            if (err) console.error('Error sending method response :\n' + err.toString());
+            else console.log('400 Response to method \'' + request.methodName + '\' sent successfully.');
+        });
+    } else {
+        // Respond the cloud app for the device method
+        response.send(200, 'Firmware update started.', function (err) {
+            if (err) console.error('Error sending method response :\n' + err.toString());
+            else {
+                console.log('200 Response to method \'' + request.methodName + '\' sent successfully.');
+
+                // Run the simulated firmware update flow
+                runFirmwareUpdateFlow(firmwareVersion, firmwareUri);
             }
         });
     }
-}; 
+}
 
-// Azure IoT Central에 연결 
-client.open(connectCallback); 
+// Simulated firmwareUpdate flow
+function runFirmwareUpdateFlow(firmwareVersion, firmwareUri) {
+    console.log('Simulating firmware update flow...');
+    console.log('> Firmware version passed: ' + firmwareVersion);
+    console.log('> Firmware URI passed: ' + firmwareUri);
+    async.waterfall([
+        function (callback) {
+            console.log("Image downloading from " + firmwareUri);
+            var patch = {
+                FirmwareUpdateStatus: 'Downloading image..'
+            };
+            reportUpdateThroughTwin(patch, callback);
+            sleep(10000, callback);
+        },
+        function (callback) {
+            console.log("Downloaded, applying firmware " + firmwareVersion);
+            deviceOnline = false;
+            var patch = {
+                FirmwareUpdateStatus: 'Applying firmware..',
+                Online: false
+            };
+            reportUpdateThroughTwin(patch, callback);
+            sleep(8000, callback);
+        },
+        function (callback) {
+            console.log("Rebooting");
+            var patch = {
+                FirmwareUpdateStatus: 'Rebooting..'
+            };
+            reportUpdateThroughTwin(patch, callback);
+            sleep(10000, callback);
+        },
+        function (callback) {
+            console.log("Firmware updated to " + firmwareVersion);
+            deviceOnline = true;
+            var patch = {
+                FirmwareUpdateStatus: 'Firmware updated',
+                Online: true,
+                Firmware: firmwareVersion
+            };
+            reportUpdateThroughTwin(patch, callback);
+            callback(null);
+        }
+    ], function (err) {
+        if (err) {
+            console.error('Error in simulated firmware update flow: ' + err.message);
+        } else {
+            console.log("Completed simulated firmware update flow");
+        }
+    });
+
+    // Helper function to update the twin reported properties.
+    function reportUpdateThroughTwin(patch, callback) {
+        console.log("Sending...");
+        console.log(JSON.stringify(patch, null, 2));
+
+        client.getTwin(function (err, twin) {
+            if (!err) {
+                twin.properties.reported.update(patch, function (err) {
+                    if (err) callback(err);
+                });
+            } else {
+                if (err) callback(err);
+            }
+        });
+    }
+
+    function sleep(milliseconds, callback) {
+        console.log("Simulate a delay (milleseconds): " + milliseconds);
+        setTimeout(function () {
+            callback(null);
+        }, milliseconds);
+    }
+}
+
+function sendTelemetry(data, schema) {
+    if (deviceOnline) {
+        var d = new Date();
+        var payload = JSON.stringify(data);
+        var message = new Message(payload);
+        message.properties.add('$$CreationTimeUtc', d.toISOString());
+        message.properties.add('$$MessageSchema', schema);
+        message.properties.add('$$ContentType', 'JSON');
+
+        //console.log('Sending device message data:\n' + payload);
+        client.sendEvent(message, printErrorFor('send event'));
+    } else {
+        console.log('Offline, not sending telemetry');
+    }
+}
+
+var client = Client.fromConnectionString(connectionString, Protocol);
+
+client.open(function (err) {
+    if (err) {
+        printErrorFor('open')(err);
+    } else {
+
+        // Create device Twin
+        client.getTwin(function (err, twin) {
+            if (err) {
+                console.error('Could not get device twin');
+            } else {
+                console.log('Device twin created');
+
+                twin.on('properties.desired.alertLED', function (delta) {
+                    // Handle desired properties set by solution
+                    console.log('Received new desired properties:' + JSON.stringify(delta));
+
+                    // TODO: LED On/Off
+                });
+
+                // Send reported properties
+                twin.properties.reported.update(reportedProperties, function (err) {
+                    if (err) throw err;
+                    console.log('Twin state reported');
+                });
+
+                // Register handlers for all the method names we are interested in.
+                // Consider separate handlers for each method.
+                client.onDeviceMethod('Reboot', onDirectMethod);
+                client.onDeviceMethod('FirmwareUpdate', onFirmwareUpdate);
+                client.onDeviceMethod('EmergencyValveRelease', onDirectMethod);
+
+                client.onDeviceMethod('AlertLEDOn', onAlertLEDOn);
+                client.onDeviceMethod('SetLEDOn', onDirectMethod);
+                client.onDeviceMethod('SetLEDOff', onDirectMethod);
+            }
+        });
+
+        // Start sending telemetry
+        var sendTemperatureInterval = setInterval(function () {
+            bme280.readSensorData()
+            .then((data) => {
+                var data = {
+                    'temperature': data.temperature_C,
+                    'temperature_unit': temperatureUnit
+                };
+                sendTelemetry(data, temperatureSchema)
+            })
+            .catch((err) => {
+              console.log(`BME280 read error: ${err}`);
+              setTimeout(readSensorData, 2000);
+            });
+        }, 5000);
+
+        var sendHumidityInterval = setInterval(function () {
+            bme280.readSensorData()
+            .then((data) => {
+                var data = {
+                    'humidity': data.humidity,
+                    'humidity_unit': humidityUnit
+                };
+                sendTelemetry(data, humiditySchema)
+            })
+            .catch((err) => {
+              console.log(`BME280 read error: ${err}`);
+              setTimeout(readSensorData, 2000);
+            });
+        }, 5000);
+
+        client.on('error', function (err) {
+            printErrorFor('client')(err);
+            if (sendTemperatureInterval) clearInterval(sendTemperatureInterval);
+            if (sendHumidityInterval) clearInterval(sendHumidityInterval);
+
+            client.close(printErrorFor('client.close'));
+        });
+    }
+});
