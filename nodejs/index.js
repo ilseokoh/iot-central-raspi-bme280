b6c55c79972c728cc466be89296590c3fddaba79
오일석 <onestone@live.co.kr>
2018-12-19 05:18:49 +0900

    del wringpi

--- nodejs/index.js
+++ nodejs/index.js
@@ -4,7 +4,6 @@ var config = require('./config.json');
 var MessageProcessor = require('./messageProcessor.js');
 
 var clientFromConnectionString = require('azure-iot-device-mqtt').clientFromConnectionString;
-var wpi = require('wiring-pi');
 
 var connectionString = config.clientFromConnectionString; 
 var client = clientFromConnectionString(connectionString); 
